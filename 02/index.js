91b160d96080b1b0fa0edba951f12f2f28af4da6
Stefan Luk <sluk@anwb.nl>
2024-03-25 21:55:52 +0100

    feat: day02 part1 solution

--- 02/index.js
+++ 02/index.js
@@ -1,14 +1,56 @@
-// import { readFileSync } from "fs";
-// const lines = readFileSync("./input.txt", { encoding: "utf-8" })
-//   .trim() // Remove starting/ending whitespace
-//   .split("\n"); // Split on newline
-//   .map() // Map each line into ... 
-// const part1 = () => {
-//   // code
-// }
-// const part2 = () => {
-//   // code
-// }
-// part1();
-// part2();
-// export {}
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { promises as fsPromises } from "fs";
+function asyncReadFile(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const max = {
+            red: 12,
+            green: 13,
+            blue: 14
+        };
+        let sum = 0;
+        try {
+            const contents = yield fsPromises.readFile(filename, "utf-8");
+            const arr = contents.split(/\r?\n/);
+            arr.forEach(line => {
+                const splittedLine = line.split(': ');
+                const id = splittedLine[0].replace('Game ', '');
+                const colors = splittedLine[1].split(/, |; /);
+                // let colorScore = {
+                //   red: 0,
+                //   green: 0,
+                //   blue: 0
+                // }
+                let isPossible = true;
+                colors.forEach(color => {
+                    const splittedColor = color.split(' ');
+                    console.log(splittedColor[0], splittedColor[1]);
+                    if (parseInt(splittedColor[0]) > max[splittedColor[1]]) {
+                        isPossible = false;
+                    }
+                    // colorScore[splittedColor[1]] = colorScore[splittedColor[1]] + parseInt(splittedColor[0]);
+                });
+                // Object.keys(colorScore).forEach(color => {
+                //   if ( colorScore[color] > max[color]) {
+                //     isPossible = false
+                //   }
+                // })
+                if (isPossible) {
+                    sum += parseInt(id);
+                }
+            });
+            console.log(sum);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
+}
+asyncReadFile("./input.txt");
--- 02/index.ts
+++ 02/index.ts
@@ -1,10 +1,37 @@
 import { promises as fsPromises} from "fs";
 
 async function asyncReadFile(filename: string) {
+  const max = {
+    red: 12,
+    green: 13,
+    blue: 14
+  };
+
+  let sum = 0;
+
   try {
     const contents = await fsPromises.readFile(filename, "utf-8");
     const arr = contents.split(/\r?\n/);
-    
+
+    arr.forEach(line => {
+      const splittedLine = line.split(': ');
+      const id = splittedLine[0].replace('Game ', '');
+      const colors = splittedLine[1].split(/, |; /);
+
+      let isPossible = true;
+      colors.forEach(color => {
+        const splittedColor = color.split(' ');
+
+        if ( parseInt(splittedColor[0]) > max[splittedColor[1]]) {
+          isPossible = false
+        }
+      })
+
+      if (isPossible) {
+        sum += parseInt(id);
+      }
+    })
+    console.log(sum);
   } catch (err) {
     console.log(err);
   }
